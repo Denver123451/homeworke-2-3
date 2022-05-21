ed23678ce84cd127c6e1f98ee4edfb48a68573cf
Denver12345 <villaan12345>
2022-05-21 21:27:18 +0300

    aded files

---
+++ index.js
@@ -0,0 +1,5 @@
+let numberOne = prompt('enter the first term');
+let numberTwo = prompt('enter the second term');
+let numb = Number(numberOne) + Number(numberTwo);
+
+alert(`result is  ${ numb }`);
